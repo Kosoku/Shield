a5f254abd17a3dcb3f59d7fd10230d7dbc0c88f1
William Towe <willbur1984@gmail.com>
2021-06-12 12:12:23 -0400

    Update copyright year

--- Shield/iOS/KSHMediaLibraryAuthorization.m
+++ Shield/iOS/KSHMediaLibraryAuthorization.m
@@ -3,7 +3,7 @@
 //  Shield
 //
 //  Created by William Towe on 4/7/17.
-//  Copyright © 2019 Kosoku Interactive, LLC. All rights reserved.
+//  Copyright © 2021 Kosoku Interactive, LLC. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
